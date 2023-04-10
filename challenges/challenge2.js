2e4d2f6a18f342cdcc4d1b9ab41757bc500d450b
scottzyang <scottzyang@gmail.com>
2023-04-09 22:42:41 -0500

    Complete challenge 2

--- challenges/challenge2.js
+++ challenges/challenge2.js
@@ -2,34 +2,58 @@
  *******************************************************************************
  * INSTRUCTIONS:
  * Follow the steps below and answer the discusssion questions that follow.
- * 
+ *
  * 1. Read over the code that follows. What will be printed to the console when
  *    it runs? Run the code using `node challenge2.js` and verify that your
  *    expectation was correct.
- * 
- * 
+ *    - Assumption:
+ *        - "Hello there, Ducky"
+ *        - "MAKE SCHOOL IS AWESOME!!!"
+ *
+ *
  * 2. What happens if greet() fails? Make it fail by changing 'name' to a number
  *    instead of a string. What happens? Does uppercaser() still run?
- * 
- * 
+ *    - If greet() rejects, the first then() will simply return a promise with the same value as the original promise, a reject.
+ *      This is because the first then() is not provided with a failure handler.
+ *    - The next then() would also return a promise with the same value as the original for the same reason.
+ *    - Finally, it'll reach the catch() block, and that'll output "Received an error" and also output the error.
+ *
+ *
  * 3. What happens if greet() succeeds and uppercaser() fails? Modify your code
  *    to achieve this result by changing the values of 'name' and 'my_str' and
  *    run the code again.
- * 
- * 
+ *    - If greet() succeeds, it'll output "Hello there, Ducky"
+ *    - It will then return the settled promise value from the uppercaser(my_str). In this case, it'll be a rejected promise.
+ *    - This rejected promise is then passed to the second .then(), which has no failure handler, so it simply returns a promise with
+ *      the same settled value as the original, in this case a rejected promise from the uppercaser(my_str) function
+ *    - This error will then be caught by the catch() function and ouput an error message and the error.
+ *
+ *
  * 4. Write a method that takes a string as input and returns the input string
  *    with a space added between each character. E.g. 'foo' -> 'f o o'
- * 
- *    Name this method spacer(str). It should run asynchronously, so use a 
+ *
+ *    Name this method spacer(str). It should run asynchronously, so use a
  *    setTimeout() and return a Promise.
- * 
+ *
  *    Last, call spacer() after you call greeter() and uppercaser().
- * 
+ *
  *    Make sure you only have one catch() block. If you have more than one,
- *    refactor your code so that you only have one. 
- * 
+ *    refactor your code so that you only have one.
+ *
  *******************************************************************************
  */
+
+ function spacer(str) {
+  return new Promise( (resolve, reject) => {
+    setTimeout(() => {
+      if (typeof str === 'string') {
+        resolve(str.split('').join(" "));
+      } else {
+        reject("Str must be a string!")
+      }
+    }, 2000);
+  });
+ }
 
  /**
   * Asynchronously returns a greeting for a specified name.
@@ -38,7 +62,7 @@
 function greet(name) {
     return new Promise(function(resolve, reject) {
       setTimeout(function() {
-        if (typeof name === 'string') { 
+        if (typeof name === 'string') {
           resolve('Hello there, ' + name);
         } else {
           reject('Name must be a string!');
@@ -63,8 +87,9 @@ function uppercaser(str) {
     });
 }
 
-name = 'Ducky'
-my_str = 'Make School is Awesome!!!'
+name = "Ducky"
+my_str = "Make school is dope!"
+str = "Hello"
 
 greet(name)
     .then((greetResult) => {
@@ -73,7 +98,12 @@ greet(name)
     })
     .then((uppercaserResult) => {
         console.log(uppercaserResult)
-    }).catch((err) => {
+        return spacer(str);
+    })
+    .then((spacerResult) => {
+      console.log(spacerResult)
+    })
+    .catch((err) => {
         console.log('Received an error!')
         console.log(err);
     });
